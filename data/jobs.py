dae4683abc1aa3800188057940a17ad1cea6280f
that's me ^_^ <77808924+Dary-so-icy@users.noreply.github.com>
2022-04-18 17:22:02 +0300

    Update jobs.py

--- data/jobs.py
+++ data/jobs.py
@@ -1,22 +1,20 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-
 from .db_session import SqlAlchemyBase
 
 
-class Jobs(SqlAlchemyBase):
-    __tablename__ = 'jobs'
+class Subject(SqlAlchemyBase):
+    __tablename__ = 'subject'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    start_date = sqlalchemy.Column(sqlalchemy.String,
-                                   default=datetime.datetime.now)
-    end_date = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    is_hard = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
+    lesson_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("lessons.id"))
     user = orm.relation('User')
+    lesson = orm.relation('Lesson')
