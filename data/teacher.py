41d4564be0c8fcf614fe925164382cf583edea3d
genia <sofia976584@gmail.com>
2022-05-02 18:43:13 +0300

    В фйле index.html изменено расположение имени вошедшего пользователя, если нажать на имя пользователя рядом со значком bootstrap совершается выход из учетной записи. Доработать: выпадающий список с кнопкой выхода. Teacher.py доп класс для учителя, добавляющего уроки

---
+++ data/teacher.py
@@ -0,0 +1,20 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Lesson(SqlAlchemyBase):
+    __tablename__ = 'lesson'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    time = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    place = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    subject_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("subject.id"))
+    user = orm.relation('User')
+    subject = orm.relation('Subject')
